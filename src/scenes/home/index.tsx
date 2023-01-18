6b414c3ea1869c024fec895e8816c611d7dd64d7
FelipeMDantas <felipemdantas@terra.com.br>
2023-01-17 23:56:31 -0300

    Items relative positioning improved.

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -18,10 +18,10 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
-        <div>
-          <div>
-            <div>
-              <div>
+        <div className="z-10 mt-32 md:basis-3/5">
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before: absolute before:-top-20 before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
