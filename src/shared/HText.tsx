ff239aea71922c1d5d152ace72f50b13d13e6247
FelipeMDantas <felipemdantas@terra.com.br>
2023-01-19 22:49:11 -0300

    Benefits page and new type developed.

--- src/shared/HText.tsx
+++ src/shared/HText.tsx
@@ -1,7 +1,9 @@
 type Props = { children: React.ReactNode };
 
 const HText = ({ children }: Props) => {
-  return <h1 className="basis-3/5 font-montserrat text-3xl font-bold"></h1>;
+  return (
+    <h1 className="basis-3/5 font-montserrat text-3xl font-bold">{children}</h1>
+  );
 };
 
 export default HText;
