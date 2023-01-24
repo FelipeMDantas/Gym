dd43fee75b5520edf21c14710337c81daf401439
FelipeMDantas <felipemdantas@terra.com.br>
2023-01-23 23:44:57 -0300

    Animations applied to ContactUs component.

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,9 +1,71 @@
+import HText from "@/shared/HText";
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  return <div>ContactUs</div>;
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) e.preventDefault();
+  };
+
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          </p>
+        </motion.div>
+
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formsubmit.co/felipemdantas@terra.com.br"
+            ></form>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
